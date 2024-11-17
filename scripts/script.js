9a8c06a0503e901f6982fda5479d6b1103ca4171
Lonozx <lonozx@gmail.com>
2024-11-17 10:20:55 +0200

    update styles

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,8 @@
 $(document).ready(function(){
     $('.plus').click(function(event){
         $(this).toggleClass('active').next().slideToggle(300);
-        // $('.question').toggleClass('whitebg').next().slideToggle(300);
-        // if($(this).hasClass('active')){
-        //     $('.question').toggleClass('whitebg').next().slideToggle(300);
-        // } else {
-        //     $('.question').toggleClass('whitebg').next().slideToggle(300);
-        // }
+        // $('.question').parent().toggleClass('whitebg');
+        $(this).closest('.question').toggleClass('whitebg');
     });
-    // $('.plus').click(function(event){
-    //     $('.question').toggleClass('whitebg').next().slideToggle(300);
-    // });
+    
 })
